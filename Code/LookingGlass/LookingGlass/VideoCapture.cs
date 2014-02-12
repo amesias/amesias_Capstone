525cf4c17190cbe21b056dbc8e3c781d1a6125db
amesias <amesias@student.neumont.edu>
2014-02-11 23:19:05 -0700

    Video capture work

---
+++ Code/LookingGlass/LookingGlass/VideoCapture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LookingGlass
+{
+    class VideoCapture
+    {
+        PictureCapturer picCapturer;
+
+        public VideoCapture()
+        {
+            picCapturer = new PictureCapturer();
+        }
+    }
+}
